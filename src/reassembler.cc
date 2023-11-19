db71e16ee9382c742efea1766d61b25bea41d9f5
qylWheels <command1748165360@126.com>
2023-11-19 11:08:35 +0800

    Update CMakeLists.txt to adjust Manjaro environment

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -2,23 +2,32 @@
 
 #include <cstdint>
 
+#include <algorithm>
+#include <iostream>
 #include <optional>
 #include <string>
 #include <vector>
 
 Reassembler::Reassembler()
-  : first_unpopped_(-1), first_unassembled_(-1), capacity_(0), buf_() {}
+  : first_unpopped_(0), first_unassembled_(0), capacity_(0), buf_(),
+    last_substring_arrived_(false), last_substring_end_(0) {}
 
 void
 Reassembler::try_insert(Writer &output)
 {
+  if (this->first_unassembled_ >= this->buf_.size()) {
+    return;
+  }
+
   if (std::nullopt == this->buf_[this->first_unassembled_]) {
     return;
   }
 
-  for (std::size_t i = this->first_unassembled_; this->buf_[i] != std::nullopt; i += 1)
+  for (std::size_t i = this->first_unassembled_;
+       i < this->buf_.size() && this->buf_[i] != std::nullopt;
+       i += 1)
   {
-    output.push(std::string{1, this->buf_[i].value()});
+    output.push(std::string(1, this->buf_[i].value()));
     this->first_unassembled_ += 1;
   }
 }
@@ -31,22 +40,35 @@ Reassembler::insert(std::uint64_t first_index, std::string data,
     return;
   }
 
-  this->first_unpopped_ = output.reader().bytes_popped();
+  if (is_last_substring) {
+    this->last_substring_arrived_ = true;
+    this->last_substring_end_ = first_index + data.length();
+  }
 
-  if (static_cast<std::int64_t>(first_index) <= this->first_unassembled_) {
+  this->first_unpopped_ = output.reader().bytes_popped();
+  this->capacity_ = this->first_unassembled_ - this->first_unpopped_ + output.available_capacity();
+
+  if (first_index <= this->first_unassembled_) {
     std::size_t before_push = output.bytes_pushed();
-    output.push(std::string{data, this->first_unassembled_ - first_index});
+    std::size_t begin_to_insert = this->first_unassembled_ - first_index;
+    if (begin_to_insert < data.length()) {
+      output.push(std::string{data, begin_to_insert});
+    }
     std::size_t exact_pushed = output.bytes_pushed() - before_push;
-    if (is_last_substring) {
+    this->first_unassembled_ += exact_pushed;
+    this->try_insert(output);
+    if (this->last_substring_arrived_
+     && this->first_unassembled_ >= this->last_substring_end_)
+    {
       output.close();
     }
-    this->first_unassembled_ += exact_pushed;
-    this->capacity_ = this->first_unassembled_ - this->first_unpopped_ + output.available_capacity();
-    this->try_insert(output);
   } else {
     std::uint64_t first_unacceptable = this->first_unpopped_ + this->capacity_;
     this->buf_.resize(first_unacceptable, std::nullopt);
-    for (std::size_t i = first_index; i < data.length() && i < first_unacceptable; i += 1) {
+    for (std::size_t i = first_index;
+         i < first_index + data.length() && i < first_unacceptable;
+         i += 1)
+    {
       this->buf_[i] = data[i - first_index];
     }
   }
@@ -56,7 +78,7 @@ std::uint64_t
 Reassembler::bytes_pending() const
 {
   std::uint64_t count = 0;
-  for (std::size_t i = this->first_unassembled_; i < this->buf_.size(); i += 1) {
+  for (std::uint64_t i = this->first_unassembled_; i < this->buf_.size(); i += 1) {
     if (std::nullopt != this->buf_[i]) {
       count += 1;
     }
